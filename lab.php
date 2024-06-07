7a14e13371c6086ab275d7e8a0117771018bc7e0
Карпов Александр <karpoffSasha03@gmail.com>
2024-06-08 02:39:22 +0500

    18

--- lab.php
+++ lab.php
@@ -5,129 +5,159 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>php-17</title>
+    <title>php-18</title>
 </head>
 <body>
+
 <?php
 echo "<h2>пункт 1</h2>";
-function makeSMTH($a, $b)
+function showInterval($start, $end)
 {
-    if ($a >= 0 && $b >= 0){
-        $res = $a-$b;
-        echo "<h2>$res</h2>>";
-    }
-    elseif ($a < 0 && $b < 0){
-        $res = $a*$b;
-        echo "<h2>$res</h2>";
-    }
-    else{
-        $res = $a+$b;
-        echo "<h2>$res</h2>";
-    }
-}
-$a = 1;
-$b = 2;
-makeSMTH($a, $b);
+    if ($start > $end)
+        exit("Неправильный интервал!");
+    $i = $start;
+    do
+    {
+        if($i == 0)
+            echo "<h2>$i - это ноль.</h2>";
+        elseif ($i%2 == 0)
+            echo "<h2>$i - четное число.</h2>";
+        else
+            echo "<h2>$i - нечетное число.</h2>";
+        $i++;
 
+    }
+    while($i < $end);
+}
+
+showInterval(0, 10);
 
 echo "<h2>пункт 2</h2>";
 
-$a = mt_rand(0, 16);
-switch ($a) {
-    case 0:
-        echo "0\n";
-    case 1:
-        echo "1\n";
-    case 2:
-        echo "2\n";
-    case 3:
-        echo "3\n";
-    case 4:
-        echo "4\n";
-    case 5:
-        echo "5\n";
-    case 6:
-        echo "6\n";
-    case 7:
-        echo "7\n";
-    case 8:
-        echo "8\n";
-    case 9:
-        echo "9\n";
-    case 10:
-        echo "10\n";
-    case 11:
-        echo "11\n";
-    case 12:
-        echo "12\n";
-    case 13:
-        echo "13\n";
-    case 14:
-        echo "14\n";
-    case 15:
-        echo "15\n";
-        break;
-}
-echo "<br>";
+$localities = ['Московская область' => array('Москва','Зеленоград','Клин'),
+    'Ленинградская область' => array('Санкт-Петербург','Всеволожск','Павловск','Кронштадт'),
+    'Тюменская область' => array('Тюмень','Ишим','Тобольск','Заводоуковск')];
 
-//Пункт3
-function sum($a, $b)
-{
-    return $a + $b;
-}
-function difference($a, $b)
-{
-    return $a - $b;
-}
-function multiplication($a, $b)
-{
-    return $a * $b;
-}
-function division($a, $b)
-{
-    return $a / $b;
+foreach ($localities as $key => $value){
+    $cities = implode(', ', $value);
+    echo "<h2>$key: </h2>";
+    echo "<h2>$cities</h2>";
 }
 
+echo "<h2>пункт 3</h2>";
+$array = [
+    'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo',
+    'ж' => 'zh', 'з' => 'z', 'и' => 'i', 'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm',
+    'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u',
+    'ф' => 'f', 'х' => 'kh', 'ц' => 'ts', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sch', 'ъ' => '',
+    'ы' => 'y', 'ь' => '', 'э' => 'e', 'ю' => 'yu', 'я' => 'ya'
+];
+
+$str = strtolower("это текст на русском языке!");
+$res = '';
+
+for ($i = 0; $i < strlen($str); $i++)
+{
+    $symbol = mb_substr($str, $i, 1);
+    if (array_key_exists($symbol, $array))
+    {
+        $res .= $array[$symbol];
+    }
+    else
+    {
+        $res .= $symbol;
+    }
+}
+
+echo "<h2>$res</h2>";
 
 echo "<h2>пункт 4</h2>";
 
-function mathOperation($arg1, $arg2, $operation){
-    switch ($operation){
-        case 'сумма':
-            return sum($arg1,$arg2);
-        case 'разность':
-            return difference($arg1,$arg2);
-        case 'умножение':
-            return multiplication($arg1,$arg2);
-        case 'деление':
-            return division($arg1,$arg2);
-        default:
-            return "Операция не определена!";
+$menu = array(
+    'name' => 'Каталог товаров',
+    'hasChildren' => true,
+    'items' => array(
+        array(
+            'name' => 'Мойки',
+            'hasChildren' => true,
+            'items' => array(
+                array(
+                    'name' => 'Первый вид мойки',
+                    'hasChildren' => false,
+                    'items' => []
+                ),
+                array(
+                    'name' => 'Второй вид мойки',
+                    'hasChildren' => true,
+                    'items' => array(
+                        array(
+                            'name' => 'Второй вид мойки образец 1',
+                            'hasChildren' => false,
+                            'items' => []
+                        ),
+                        array(
+                            'name' => 'Второй вид мойки образец 2',
+                            'hasChildren' => false,
+                            'items' => []
+                        )
+                    )
+                )
+            )
+        ),
+        array(
+            'name' => 'Фильтры',
+            'hasChildren' => false,
+            'items' => []
+        )
+    )
+
+);
+
+function renderParent($data): string
+{
+    $parent = '';
+    $name = $data['name'];
+    $parent .= "<li>$name<ul>";
+    foreach ($data['items'] as $element){
+        if ($element['hasChildren']){
+            $parent .= renderParent($element);
+        }
+        else{
+            $parent .= renderChildren($element);
+        }
     }
+    $parent .= "</li></ul>";
+    return $parent;
 }
-$result_operation = mathOperation(10, 10, "умножение");
-echo "<h2>$result_operation</h2>";
+function renderChildren($data): string
+{
+    $name = $data['name'];
+    return "<li>$name</li>";
+}
 
+$result_tree = renderParent($menu);
 
-echo "<h2>пункт 5</h2>";
-$one = date('Y');
-echo "Способ 1: <h2>$one</h2>";
-$two = localtime(null, true)['tm_year'] + 1900;
-echo "Способ 2: <h2>$two</h2>";
-$three = getdate()['year'];
-echo "Способ 3: <h2>$three</h2>";
+echo "<ul>$result_tree</ul>";
+
 
 echo "<h2>пункт 6</h2>";
-function power($val, $pow)
-{
-    if($pow == 1){
-        return $val;
+foreach ($localities as $key => $value){
+    $cities = [];
+    foreach ($value as $city){
+        if (str_starts_with(mb_strtolower($city), 'к'))
+            $cities[] = $city;
     }
-    return $val * power($val, $pow-1);
-}
+    echo "<h2>$key: </h2>";
+    if (count($cities) == 0)
+        echo "<h2>В этой области отсуствуют города на 'К'</h2>";
+    else
+    {
+        $result = implode($cities);
+        echo "<h2>$result</h2>";
+    }
 
-$result_pow = power(2, 10);
-echo "<h2>$result_pow</h2>";
+}
 ?>
+
 </body>
 </html>
